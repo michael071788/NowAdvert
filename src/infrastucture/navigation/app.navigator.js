2984f90a7dceeb96d8803bd2c85d89f205bef4de
michael071788 <michael071788@gmail.com>
2022-07-06 21:02:17 +0400

    feat :tada: added temporary icons for bottom tab nav using Ionicons

--- src/infrastucture/navigation/app.navigator.js
+++ src/infrastucture/navigation/app.navigator.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import { AdvertNavigator } from "./advert.navigator";
 import { HomeNavigator } from "./home.navigator";
@@ -7,8 +8,23 @@ import { ProfileNavigator } from "./profile.navigator";
 
 const Tab = createBottomTabNavigator();
 
+const TAB_ICON = {
+  Home: "home-outline",
+  Advert: "videocam-outline",
+  Profile: "person-outline",
+};
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
+
 export const AppNavigator = () => (
-  <Tab.Navigator>
+  <Tab.Navigator screenOptions={createScreenOptions}>
     <Tab.Screen name="Home" component={HomeNavigator} />
     <Tab.Screen name="Advert" component={AdvertNavigator} />
     <Tab.Screen name="Profile" component={ProfileNavigator} />
