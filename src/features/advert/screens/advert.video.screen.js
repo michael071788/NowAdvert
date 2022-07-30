7367effe5e05fb7e79412c8aa67ec6b4b48b74a7
michael071788 <michael071788@gmail.com>
2022-07-31 01:44:53 +0400

    feat :tada: clean advert video screen style

--- src/features/advert/screens/advert.video.screen.js
+++ src/features/advert/screens/advert.video.screen.js
@@ -1,12 +1,16 @@
 import React, { useRef, useState } from "react";
-import {
-  View,
-  StyleSheet,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-} from "react-native";
+import { View, TouchableWithoutFeedback, TouchableOpacity } from "react-native";
 import { Video } from "expo-av";
-import { ButtonContainer } from "../../../infrastucture/theme/styles/advert.screen.style";
+import {
+  MainScreenView,
+  VideoStyle,
+  VideoButtonContainer,
+  Container1,
+  Container2,
+  Container3,
+  BackButtonContainer,
+  PlayButtonContainer,
+} from "../../../infrastucture/theme/styles/advert.video.screen.style";
 import { UsedPrimaryAppContext } from "../../../services/primary.app.provider";
 
 export const AdvertVideoScreen = ({ route, navigation }) => {
@@ -27,31 +31,19 @@ export const AdvertVideoScreen = ({ route, navigation }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={{ flex: 1 }}>
-        <Video
-          ref={video}
-          style={styles.video}
-          source={{
-            uri: videoURI,
-          }}
-          resizeMode="cover"
-          onPlaybackStatusUpdate={(e) => setStatus(e)}
-        />
-      </View>
-
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0,
+    <MainScreenView>
+      <Video
+        ref={video}
+        style={VideoStyle}
+        source={{
+          uri: videoURI,
         }}
-      >
-        <View style={{ flex: 1, alignItems: "flex-start" }}>
+        resizeMode="cover"
+        onPlaybackStatusUpdate={(e) => setStatus(e)}
+      />
+
+      <VideoButtonContainer>
+        <Container1>
           <TouchableOpacity
             onPress={() => {
               primaryContext.ShowUserProfileBar(true);
@@ -59,54 +51,23 @@ export const AdvertVideoScreen = ({ route, navigation }) => {
             }}
             activeOpacity={0.8}
           >
-            <View style={{ margin: 10 }}>
-              <ButtonContainer
-                name={"CARETLEFT"}
-                size={30}
-                bgcolor={"rgba(105, 105, 105, 0.37)"}
-              />
-            </View>
+            <BackButtonContainer />
           </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            flex: 1,
-            alignItems: "center",
-            flexDirection: "column-reverse",
-          }}
-        >
+        </Container1>
+        <Container2>
           <TouchableWithoutFeedback
             onPressIn={onPlayPressInOut}
             onPressOut={onPlayPressInOut}
           >
             <View>
-              <ButtonContainer
+              <PlayButtonContainer
                 name={status.isPlaying ? "PAUSE" : "CARETRIGHT"}
-                size={80}
-                bgcolor={"rgba(0, 0, 0, 0.25)"}
-                iconcolor={"black"}
-                iconsize={40}
               />
             </View>
           </TouchableWithoutFeedback>
-        </View>
-        <View style={{ flex: 1 }} />
-      </View>
-    </View>
+        </Container2>
+        <Container3 />
+      </VideoButtonContainer>
+    </MainScreenView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  video: {
-    width: "100%",
-    height: "100%",
-  },
-  buttons: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
