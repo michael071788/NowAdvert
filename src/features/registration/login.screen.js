34bb0952409c35c6a5d0ba88b965a693abd3ea0c
mack-daniel <mack.duromal09@gmail.com>
2022-11-04 05:05:46 +0800

    initial auth commit

--- src/features/registration/login.screen.js
+++ src/features/registration/login.screen.js
@@ -1,167 +1,307 @@
-import { View, ScrollView, Text, SafeAreaView, TextInput } from "react-native";
-import { Button } from "react-native-paper";
-import UsedTheme from "../../../infrastucture/theme/use.theme";
-import { CommonScreenView } from "../../../infrastucture/theme/styles/container.screen.style";
-import { tabBarTotalHeight } from "../../infrastucture/theme/styles/app.navigator.style";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import UsedTheme from "../../infrastucture/theme/use.theme";
+import {
+  HeaderText,
+  Input,
+} from "../../infrastucture/theme/styles/auth.components";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+
+// cannot POST user yet
+// const BASE_URL = "https://protected-fjord-83078.herokuapp.com";
+// const BASE_URL = "https://nowadvert-api.herokuapp.com";
+
+const axiosInstance = axios.create({
+  baseURL: "https://nowadvert-api.herokuapp.com",
+});
 
 const Login = ({ navigation }) => {
+  const [userInfo, setUserInfo] = useState("");
+
   const theme = UsedTheme();
 
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = async (userData) => {
+    console.log("Pressed");
+    console.log(userData);
+
+    try {
+      const result = await axiosInstance
+        .post("/api/login", userData)
+        .then((result) => {
+          if (result.data.message) {
+            console.log(result.data.message);
+            alert(result.data.message);
+          } else if (result.status === 200) {
+            const userData = result.config.data;
+            alert("Success");
+            AsyncStorage.setItem("userInfo", JSON.stringify(userData));
+            // navigation.navigate("Login");
+          }
+        });
+    } catch (error) {
+      console.log(error.response.data);
+    }
+
+    // fetch(`${BASE_URL}/api/login`, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(userData),
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     let userInfo = data;
+
+    //     if (data.message) {
+    //       alert(data.message);
+    //     } else if (data.message === "User Login successfully") {
+    //       setUserInfo(data);
+    //       AsyncStorage.setItem("userInfo", userInfo);
+    //     }
+    //     console.log(userInfo);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error.response.data);
+    //   });
+  };
   return (
-    <CommonScreenView theme={theme} paddingBottom={tabBarTotalHeight + 10}>
-      <ScrollView vertical showsHorizontalScrollIndicator={false}>
-        {/* start input field */}
-        <SafeAreaView
+    <View
+      style={{
+        flex: 1,
+        paddingHorizontal: 10,
+      }}
+    >
+      <ScrollView scrollEnabled={false} showsVerticalScrollIndicator={false}>
+        <View
           style={{
-            width: "100%",
-            flex: 1,
-            paddingHorizontal: 20,
-            paddingTop: 120,
+            flex: 5,
+            // alignItems: "center",
+            paddingVertical: 80,
           }}
         >
-          <View>
-            <View>
-              <Text
-                style={{
-                  fontFamily: theme.typography.PRIMARY,
-                  fontWeight: "bold",
-                  fontSize: 30,
-                  textTransform: "uppercase",
-                }}
-              >
-                login
-              </Text>
-              <Text
-                style={{
-                  fontFamily: theme.typography.PRIMARY,
-                  fontSize: 12,
-                  fontWeight: "bold",
-                  textTransform: "uppercase",
-                }}
-              >
-                watch ads to win exciting prizes
-              </Text>
-            </View>
-            <View style={{ marginVertical: 18 }}>
-              <View style={{ width: "100%", marginVertical: 5 }}>
-                <TextInput
-                  style={{
-                    fontFamily: theme.typography.PRIMARY,
-                    borderRadius: 20,
-                    padding: 2,
-                    paddingHorizontal: 20,
-                    fontSize: 14,
-                    backgroundColor: "#ccc",
-                    fontWeight: "bold",
-                  }}
-                  placeholder="EMAIL ADDRESS"
-                  placeholderTextColor="#aaa"
-                />
-              </View>
-
-              <View style={{ width: "100%", marginVertical: 10 }}>
-                <TextInput
-                  style={{
-                    fontFamily: theme.typography.PRIMARY,
-                    borderRadius: 20,
-                    padding: 2,
-                    paddingHorizontal: 20,
-                    fontSize: 14,
-                    backgroundColor: "#ccc",
-                    fontWeight: "bold",
-                  }}
-                  placeholder="PASSWORD"
-                  secureTextEntry={true}
-                  placeholderTextColor="#aaa"
-                />
-              </View>
-              <Text
-                style={{
-                  fontFamily: theme.typography.PRIMARY,
-                  textTransform: "uppercase",
-                  alignSelf: "flex-end",
-                }}
-              >
-                forgot password?
-              </Text>
-            </View>
-          </View>
-
-          {/* button */}
-          <View>
-            <Button mode="contained" buttonColor="#333">
-              LOGIN
-            </Button>
-
-            <View
-              style={{
-                marginVertical: 10,
-              }}
-            >
-              <Text
-                style={{
-                  fontFamily: theme.typography.PRIMARY,
-                  textAlign: "center",
-                }}
-              >
-                - OR -
-              </Text>
-            </View>
-
-            <View style={{ marginBottom: 10 }}>
-              <Button
-                mode="outlined"
-                color="#000"
-                textColor="#000"
-                style={{ marginBottom: 20 }}
-                onPress={() => alert("pressed")}
-              >
-                CONTINUE WITH GOOGLE
-              </Button>
-            </View>
-
-            <View style={{ marginBottom: 10 }}>
-              <Button
-                mode="outlined"
-                color="#000"
-                textColor="#000"
-                style={{ marginBottom: 20 }}
-                onPress={() => alert("pressed")}
-              >
-                CONTINUE WITH APPLE
-              </Button>
-            </View>
-          </View>
           <View
             style={{
-              height: 100,
+              flex: 1,
+              justifyContent: "flex-end",
+              alignItems: "baseline",
+              marginBottom: 5,
+            }}
+          >
+            {/* header text */}
+            <HeaderText
+              title="Login"
+              subtitle="WATCH ADS TO WIN EXCITING PRIZES"
+            />
+
+            <View style={{ flex: 3 }}>
+              {/* email input */}
+              <Controller
+                name="email"
+                control={control}
+                rules={{
+                  required: "This is required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: "Email is invalid",
+                  },
+                }}
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="EMAIL ADDRESS"
+                    onChangeText={onChange}
+                    value={value}
+                  />
+                )}
+              />
+              <View style={{ height: 18, fontSize: 15 }}>
+                {errors.email ? (
+                  <>
+                    {errors.email.type === "required" && (
+                      <Text style={{ color: "red" }}>
+                        {errors.email.message}
+                      </Text>
+                    )}
+                    {errors.email.type === "pattern" && (
+                      <Text style={{ color: "red" }}>
+                        {errors.email.message}
+                      </Text>
+                    )}
+                  </>
+                ) : null}
+              </View>
+
+              {/* PASSWORD */}
+              <Controller
+                name="password"
+                control={control}
+                rules={{
+                  required: "This is required",
+                }}
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="Password"
+                    onChangeText={onChange}
+                    value={value}
+                    secureTextEntry={true}
+                  />
+                )}
+              />
+              <View style={{ height: 18, fontSize: 15 }}>
+                {errors.password && (
+                  <Text style={{ color: "red" }}>This is required.</Text>
+                )}
+              </View>
+              {/* FORGOT PASSWORD */}
+              <View
+                style={{
+                  width: "100%",
+                  flexDirection: "row",
+                  justifyContent: "flex-end",
+                  marginVertical: 10,
+                }}
+              >
+                <TouchableOpacity>
+                  <Text
+                    style={{
+                      color: "#000",
+                      fontFamily: theme.typography.PRIMARY,
+                    }}
+                  >
+                    FORGOT PASSWORD?
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              {/* LOGIN BUTTON */}
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "#333",
+                  borderRadius: 20,
+                  paddingVertical: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                onPress={handleSubmit(onSubmit)}
+              >
+                <Text
+                  style={{
+                    color: "#fff",
+                    fontFamily: theme.typography.PRIMARY,
+                  }}
+                >
+                  LOGIN
+                </Text>
+              </TouchableOpacity>
+
+              <Text
+                style={{
+                  textAlign: "center",
+                  marginVertical: 10,
+                  fontFamily: theme.typography.PRIMARY,
+                }}
+              >
+                - OR -{" "}
+              </Text>
+
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "#fff",
+                  borderWidth: 1,
+                  borderColor: "#111",
+                  borderRadius: 20,
+                  paddingVertical: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginBottom: 10,
+                }}
+                //   onPress={handleSubmit(onSubmit)}
+              >
+                <Text
+                  style={{
+                    color: "#111",
+                    fontFamily: theme.typography.PRIMARY,
+                  }}
+                >
+                  CONTINUE WITH GOOGLE
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "#fff",
+                  borderWidth: 1,
+                  borderColor: "#111",
+                  borderRadius: 20,
+                  paddingVertical: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                //   onPress={handleSubmit(onSubmit)}
+              >
+                <Text
+                  style={{
+                    color: "#111",
+                    fontFamily: theme.typography.PRIMARY,
+                  }}
+                >
+                  CONTINUE WITH APPLE
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+
+        {/* signup link */}
+        <View
+          style={{
+            flex: 1,
+            // justifyContent: "flex-end",
+          }}
+        >
+          <View
+            style={{
+              height: 40,
               flexDirection: "row",
               alignItems: "flex-end",
               justifyContent: "center",
             }}
           >
             <Text
-              style={{ fontFamily: theme.typography.PRIMARY, color: "#aaa" }}
-            >
-              NEW USER?{" "}
-            </Text>
-
-            <Button
-              mode="contained"
-              onPress={() => navigation.navigate("SIGNUP")}
-              buttonColor="#000"
               style={{
-                width: "50%",
                 fontFamily: theme.typography.PRIMARY,
+                color: "#aaa",
+                textTransform: "uppercase",
+                marginRight: 5,
               }}
             >
-              SIGN UP
-            </Button>
+              NEW USER?
+            </Text>
+            <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+              <Text style={{ fontFamily: theme.typography.PRIMARY }}>
+                SIGN UP
+              </Text>
+            </TouchableOpacity>
           </View>
-        </SafeAreaView>
-        {/* end of button */}
+        </View>
       </ScrollView>
-    </CommonScreenView>
+    </View>
   );
 };
 export default Login;
---
+++ src/infrastucture/theme/styles/auth.components.js
@@ -0,0 +1,53 @@
+import { TextInput, View, Text } from "react-native";
+import UsedTheme from "../use.theme";
+
+export const Input = (props) => {
+  const theme = UsedTheme();
+  return (
+    <View style={{ flexDirection: "row" }}>
+      <TextInput
+        style={{
+          flex: 1,
+          // height: 40,
+          marginVertical: 5,
+          fontFamily: theme.typography.PRIMARY,
+          borderRadius: 20,
+          paddingHorizontal: 20,
+          paddingVertical: 5,
+          fontSize: 16,
+          backgroundColor: "#ccc",
+        }}
+        value={props.value}
+        onChangeText={props.onChangeText}
+        {...props}
+      />
+    </View>
+  );
+};
+
+export const HeaderText = ({ title, subtitle }) => {
+  const theme = UsedTheme();
+  return (
+    <View>
+      <Text
+        style={{
+          fontSize: 26,
+          textTransform: "uppercase",
+          fontFamily: theme.typography.PRIMARY,
+        }}
+      >
+        {title}
+      </Text>
+      <Text
+        style={{
+          fontSize: 16,
+          fontFamily: theme.typography.PRIMARY,
+          marginTop: 10,
+          textTransform: "uppercase",
+        }}
+      >
+        {subtitle}
+      </Text>
+    </View>
+  );
+};
