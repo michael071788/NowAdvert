4bc3c0034286aa9bfac8fc797b4d179eb138ae60
mack-daniel <mack.duromal09@gmail.com>
2022-11-15 04:26:05 +0800

    initial

--- src/features/advert/screens/advert.screen.js
+++ src/features/advert/screens/advert.screen.js
@@ -32,6 +32,8 @@ export const AdvertScreen = ({ navigation }) => {
   const [isPreloading, setIsPreloading] = useState(true);
   const [advertListData, setAdvertListData] = useState([]);
 
+  const [count, setCount] = useState(0);
+
   const theme = UsedTheme();
 
   const primaryContext = UsedPrimaryAppContext();
@@ -120,7 +122,17 @@ export const AdvertScreen = ({ navigation }) => {
                     borderRadius: 30,
                   }}
                 >
-                  <ButtonContainer name={"HEART"} label={"1.5k"} />
+                  <ButtonContainer
+                    name={"HEART"}
+                    // label={"1.5k"}
+                    label={count}
+                    onpress={() => {
+                      console.log(item._id);
+
+                      // setCount(count + 1);
+                    }}
+                  />
+
                   <ButtonContainer name={"EYE"} label={"300"} />
                   <ButtonContainer
                     name={"SHARE"}
