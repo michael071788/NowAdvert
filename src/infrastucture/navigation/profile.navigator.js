59a06c6796752ddef383636016df6e76d5b5b9dd
mack-daniel <mack.duromal09@gmail.com>
2022-12-20 18:01:19 +0800

    initial

--- src/infrastucture/navigation/profile.navigator.js
+++ src/infrastucture/navigation/profile.navigator.js
@@ -15,6 +15,7 @@ import { List, Avatar } from "react-native-paper";
 import { useTranslation } from "react-i18next";
 import { UsedUserAuthInfoContext } from "../../services/user.auth.provider";
 import * as ImagePicker from "expo-image-picker";
+import { AxiosInstance } from "../../utils";
 
 const ProfileStack = createStackNavigator();
 
@@ -50,8 +51,44 @@ export const ProfileNavigator = ({ navigation }) => {
     console.log(result);
 
     if (!result.cancelled) {
+      try {
+        await pickImage(result);
+      } catch (error) {
+        console.log(error);
+      }
       setImage(result.uri);
     }
+  };
+
+  const pickImage = async (image) => {
+    const formdata = new FormData();
+    // formdata.append("image", imageUpload);
+    formdata.append("image", {
+      // name: new Date() + "_profile",
+      name: "image",
+      uri: image.uri,
+      type: "image/jpg",
+    });
+
+    await AxiosInstance.post(
+      // "/profile-image/6391ea8eca6fe490470ee54f",
+      "/upload",
+      formdata,
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    )
+      .then((res) => {
+        // then print response status
+        console.log(res);
+        console.log("Uploaded");
+      })
+      .catch((err) => {
+        console.log("err, ", err);
+      });
   };
 
   return (
